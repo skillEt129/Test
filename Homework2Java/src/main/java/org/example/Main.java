688dd09cb4b303f2724e3eda45e6b22ab87003b4
Roman Popov <110326564+skillEt129@users.noreply.github.com>
2022-12-16 20:55:59 +0400

    Add files via upload

---
+++ Homework2Java/src/main/java/org/example/Main.java
@@ -0,0 +1,113 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * 1) Дана последовательность N целых чисел.
+         *    Найти количество положительных чисел,
+         *    после которых следует отрицательное число.
+         */
+//
+//        int a, b, sum = 0, n;
+//        Scanner sc = new Scanner(System.in);
+//        n = sc.nextInt();
+//        a = sc.nextInt();
+//        for (int i = 1; i < n; i++) {
+//            b = sc.nextInt();
+//            if ((b < 0) && (a > 0)) ;{
+//                sum += b;
+//            }
+//            a = b;
+//        }
+//        System.out.println(sum);
+//        sc.close();
+        /**
+         * 2) Дана последовательность целых чисел, оканчивающаяся нулем.
+         *    Найти сумму положительных чисел,
+         *    после которых следует отрицательное число
+         */
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        int s = 0;
+//
+//        while (true) {
+//            int x = sc.nextInt();
+//            if (x > 0) s += x;
+//            else break;
+//        }
+//
+//        System.out.println(s);
+//
+//        sc.close();
+
+        /**
+         * 3) Дана последовательность N целых чисел.
+         *    Найти сумму простых чисел.
+         */
+
+//        int a, b, sum = 0, n;
+//        Scanner sc = new Scanner(System.in);
+//        n = sc.nextInt();
+//        a = sc.nextInt();
+//        for (int i = 1; i < n; i++) {
+//            b = sc.nextInt();
+//            if ((b % b == 0)&&(a % a == 0));
+//            {
+//                sum += b;
+//            }
+//            a = b;
+//        }
+//        System.out.println(sum);
+//        sc.close();
+
+        /**
+         * 4) Дана последовательность из N целых чисел.
+         * Верно ли, что последовательность является возрастающей.
+         */
+//                int a, b, n;
+//        Scanner sc = new Scanner(System.in);
+//        n = sc.nextInt();
+//        a = sc.nextInt();
+//        for (int i = 1; i < n; i++) {
+//            b = sc.nextInt();
+//            if (b > a)
+//            {
+//                System.out.println("да");
+//            }else{
+//                System.out.println("нет");
+//            }
+//            a = b;
+//        }
+//        sc.close();
+
+        /**
+         * 5) Дан массив целых чисел. Найти сумму элементов,
+         *    у которых последняя и предпоследняя цифры равны.
+         */
+
+//        int k = 0;
+//
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int[] mass = new int[n];
+//
+//        for (int i = 0; i < n; i++) {
+//            mass[i] = sc.nextInt();
+//        }
+//
+//
+//        for (int i = 0; i < n - 1; i++) {
+//            if (mass[i] == mass[i + 1]) {
+//                k++;
+//            }
+//        }
+//        System.out.println("k="+k);
+//        sc.close();
+
+
+    }
+}
+
